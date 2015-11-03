c5ea2ae3e21f56ac609b49a8c9bb4ee6a9ffe95b
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-11-03 02:09:55 -0200

    By Arthur: Adicionado fragments da paginaInicial e do Hemocentro.

---
+++ src/CosangueApp/app/src/main/java/tcc/cosangueapp/fragments/HemocentroFragment.java
@@ -0,0 +1,64 @@
+package tcc.cosangueapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import tcc.cosangueapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HemocentroFragment extends Fragment implements OnMapReadyCallback {
+
+
+    private GoogleMap googleMap;
+
+
+    public HemocentroFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hemocentro, container, false);
+    }
+
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            this.googleMap = googleMap;
+
+            //if(localizacao do evento != null) { executa tudo
+            // ativa botão pra pegar localização atual
+            googleMap.setMyLocationEnabled(true);
+
+                                        //(evento.endereco.latitude, evento.endereco.longitudo)
+            LatLng localizacao = new LatLng(-29.8002047, -51.124366899999984);
+
+
+            //Posiciona o mapa na coordenada desejada (zoom = 13)
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(localizacao, 13);
+            googleMap.moveCamera(update);
+
+            // Marcador no local que o mapa vai dar zoom
+            googleMap.addMarker(new MarkerOptions().title("Minha casa").snippet("Isso é o snippet").position(localizacao));
+
+            //tipo de mapa
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            
+    }
+}
--- src/CosangueApp/app/src/main/java/tcc/cosangueapp/fragments/PaginaInicialFragment.java
+++ src/CosangueApp/app/src/main/java/tcc/cosangueapp/fragments/PaginaInicialFragment.java
@@ -1,30 +1,45 @@
-package tcc.cosangueapp;
+package tcc.cosangueapp.fragments;
 
 
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import tcc.cosangueapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class PaginaInicialFragment extends Fragment {
 
+    private OnFragmentInteractionListener mListener;
 
     public PaginaInicialFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pagina_inicial, container, false);
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
 
 }
