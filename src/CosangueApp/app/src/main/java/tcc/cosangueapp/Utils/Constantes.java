779cc7eb509bdcc0c5493c5e32e2ff04d83d2082
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-11-13 14:22:09 -0200

    By Arthur: Alteradas urls de acesso ao CosangueRESTful.

--- src/CosangueApp/app/src/main/java/tcc/cosangueapp/Utils/Constantes.java
+++ src/CosangueApp/app/src/main/java/tcc/cosangueapp/Utils/Constantes.java
@@ -2,14 +2,16 @@ package tcc.cosangueapp.utils;
 
 public interface Constantes {
 
-    final String URL_USUARIO_LOGIN = "http://192.168.0.103:8080/CosangueRESTful/usuario/{login}/{senha}";
-    final String URL_USUARIO = "http://192.168.0.103:8080/CosangueRESTful/usuario";
-    final String URL_USUARIO_ID = "http://192.168.0.103:8080/CosangueRESTful/usuario/{id}";
-    final String URL_USUARIO_ATUALIZA_REG_ID = "http://192.168.0.103:8080/CosangueRESTful/usuario/{id}/{registration_id}";
+
+    final String URL_USUARIO_LOGIN = "http://projetos.sapucaia.ifsul.edu.br:8080/CosangueRESTful/usuario/{login}/{senha}";
+   // final String URL_USUARIO_LOGIN = "http://192.168.0.100:8080/CosangueRESTful/usuario/{login}/{senha}";
+    final String URL_USUARIO = "http://projetos.sapucaia.ifsul.edu.br:8080/CosangueRESTful/usuario";
+    final String URL_USUARIO_ID = "http://projetos.sapucaia.ifsul.edu.br:8080/CosangueRESTful/usuario/{id}";
+    final String URL_USUARIO_ATUALIZA_REG_ID = "http://projetos.sapucaia.ifsul.edu.br:8080/CosangueRESTful/{id}/{registration_id}";
 
 
-    final String URL_ACAO = "http://192.168.0.103:8080/CosangueRESTful/acao";
-    final String URL_ACAO_ID = "http://192.168.0.103:8080/CosangueRESTful/acao/{id}";
+    final String URL_ACAO = "http://projetos.sapucaia.ifsul.edu.br:8080/CosangueRESTful/acao";
+    final String URL_ACAO_ID = "http://projetos.sapucaia.ifsul.edu.br:8080/CosangueRESTful/acao/{id}";
 
 
 
