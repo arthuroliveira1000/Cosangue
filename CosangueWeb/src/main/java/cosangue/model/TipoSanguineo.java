1be0cc1f34ba95639cb5d2a54bddc717b7a50f3c
Luana Leite Rodrigues <luanaleiter@hotmail.com>
2015-11-19 01:07:49 -0200

    By Luana: tipo sanguineo "todos"

--- CosangueWeb/src/main/java/cosangue/model/TipoSanguineo.java
+++ CosangueWeb/src/main/java/cosangue/model/TipoSanguineo.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlEnum;
 public enum TipoSanguineo {
 
     A_POSITIVO("A+"), A_NEGATIVO("A-"), B_POSITIVO("B+"), B_NEGATIVO("B-"), AB_POSITIVO(
-            "AB+"), AB_NEGATIVO("AB-"), O_POSITIVO("O+"), O_NEGATIVO("O-"), NAO_SEI("Todos");;
+            "AB+"), AB_NEGATIVO("AB-"), O_POSITIVO("O+"), O_NEGATIVO("O-"), NAO_SEI("Todos");
 
     private String descricao;
 
